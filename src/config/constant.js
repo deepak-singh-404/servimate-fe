6b88c462ba272ca91e16e17ec4a005ebc31c9dbb
deepak-singhh <deepakshivam1998@gmail.com>
2022-01-19 22:11:43 +0530

    ebs prod url added

--- src/config/constant.js
+++ src/config/constant.js
@@ -1,9 +1,10 @@
 const prod_url = "https://servimate-admin.herokuapp.com/"
+const ebs_prod_url = "http://s-dev-env-admin.eba-hzkifsjt.us-east-1.elasticbeanstalk.com/"
 const local_url = "http://localhost:4000/"
 const partnerWalletActionTypes = [{ "Add": "add" }, { "Subtract": "sub" }, { "Reset": "reset" }]
 
 module.exports = {
-    prod_url,
+    prod_url: ebs_prod_url,
     local_url,
     partnerWalletActionTypes
 }
