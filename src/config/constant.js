d98fb0871d8277a977f94c05ca7a7b178e541072
deepak.singhh <deepakshivam1998@gmail.com>
2022-01-19 22:41:28 +0530

    undo

--- src/config/constant.js
+++ src/config/constant.js
@@ -1,10 +1,9 @@
 const prod_url = "https://servimate-admin.herokuapp.com/"
-const ebs_prod_url = "http://s-dev-env-admin.eba-hzkifsjt.us-east-1.elasticbeanstalk.com/"
 const local_url = "http://localhost:4000/"
 const partnerWalletActionTypes = [{ "Add": "add" }, { "Subtract": "sub" }, { "Reset": "reset" }]
 
 module.exports = {
-    prod_url: ebs_prod_url,
+    prod_url,
     local_url,
     partnerWalletActionTypes
 }
