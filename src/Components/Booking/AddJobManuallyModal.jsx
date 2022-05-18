41285bf7c9e0cc673d4e44f923016982199fe43f
deepak-singhh <deepakshivam1998@gmail.com>
2022-05-18 22:18:47 +0530

    addjobmanually1

--- src/Components/Booking/AddJobManuallyModal.jsx
+++ src/Components/Booking/AddJobManuallyModal.jsx
@@ -4,28 +4,95 @@ import { Form, Button, Modal } from 'react-bootstrap'
 import Loader from '../Loader'
 import { getServiceCategories, getServicesByServiceCategory } from '../../redux/actions/serviceAction'
 import { Typeahead } from 'react-bootstrap-typeahead';
+import { getServiceProviders } from "../../redux/actions/serviceProvider";
 
 const AddJobManuallyModal = ({ addManualJobModal, setAddManulJobModal }) => {
+
+    //Redux Data
     const reduxData = useSelector(store => store)
     const { serviceRoot, serviceProviderRoot } = reduxData
     const { serviceCategories, services, loader } = serviceRoot
+    let serviceProviders = serviceProviderRoot.serviceProviders
+
+    //Component States
+    let [serviceP, setServiceP] = useState([])
+    const [serviceProvider, setServiceProvider] = useState([]);
     const [serviceCategory, setServiceCategory] = useState("")
+    const [zipcode, setZipcode] = useState("")
+    const [price, setPrice] = useState("")
+    const [_services, _setServices] = useState([]);
+
     const dispatch = useDispatch()
-    const [name, setName] = useState("")
+
+    // useEffect(()=>{
+    //     if (_services.length > 0){
+    //         console.log("_services",_services)
+
+    //     }
+    // },[_services])
 
     const formHandler = (e) => {
         e.preventDefault()
     }
-
+    
+    //Handle Sevices (Get Service by Service Category)
     useEffect(() => {
-        if (serviceCategories.length > 0 && serviceCategory) {
+        if (serviceCategories.length > 0 && serviceCategory && serviceCategory !== "Select") {
             dispatch(getServicesByServiceCategory(serviceCategory))
         }
     }, [serviceCategory])
 
     useEffect(() => {
         dispatch(getServiceCategories())
+        dispatch(getServiceProviders())
+        if (serviceProviders) {
+            setServiceP(serviceProviders)
+        }
     }, [])
+
+    useEffect(() => {
+        if (serviceProviders.length > 0) {
+            //Get the zipcode and serviceCategory from current booking.
+            let bookingZipcode = zipcode
+            //const bookingServiceCategory = currentBooking.services[0].serviceCategory
+            //Filter all service providers who serve above service at above location.
+
+            let _serviceProviders = []
+            for (const sp of serviceProviders) {
+                let serviceCategoryMatch = false
+                let zipcodeMatch = false
+
+                //Handle serviceCategory
+                // if (bookingServiceCategory) {
+                //     for (const sc of sp.serviceCategoryId) {
+                //         if (sc._id == bookingServiceCategory) {
+                //             serviceCategoryMatch = true
+                //         }
+                //     }
+                // }
+
+                //Handler pincode
+                if (bookingZipcode && bookingZipcode.length == 6) {
+                    for (const zc of sp.zipcodes) {
+                        if (zc == bookingZipcode) {
+                            zipcodeMatch = true
+                        }
+                    }
+                }
+
+                if (zipcodeMatch) {
+                    _serviceProviders.push(sp)
+                }
+
+            }
+            setServiceP(_serviceProviders)
+
+            //Handle If no zipcode selected.
+            if (zipcode === "") {
+                setServiceP(serviceProviders)
+            }
+        }
+    }, [serviceProviders, zipcode])
 
     return (
         <>
@@ -44,6 +111,38 @@ const AddJobManuallyModal = ({ addManualJobModal, setAddManulJobModal }) => {
                                     <option value={c._id}>{c.name}</option>
                                 ) : null}
                             </Form.Control>
+                        </Form.Group>
+
+                        <Form.Group >
+                            <Form.Label>Service</Form.Label>
+                            <Typeahead
+                                id="basic-typeahead-multiple"
+                                labelKey="serviceName"
+                                multiple
+                                onChange={_setServices}
+                                options={services}
+                                placeholder="Choose Service ..."
+                                selected={_services}
+                            />
+                        </Form.Group>
+
+                        <Form.Group >
+                            <Form.Label>Pin Code</Form.Label>
+                            <Form.Control value={zipcode} onChange={(e) => setZipcode(e.target.value)} type="text" />
+                        </Form.Group>
+
+                        <Form.Group >
+                            <Form.Label>Service Provider</Form.Label>
+
+                            <Typeahead
+                                id="basic-typeahead-single"
+                                labelKey="name"
+                                onChange={setServiceProvider}
+                                options={serviceP}
+                                placeholder="Choose ServiceProvider ..."
+                                selected={serviceProvider}
+                            />
+
                         </Form.Group>
 
                         {/* {serviceProviderRoot.loader ? <Loader /> : <Button variant="primary" type="submit">
