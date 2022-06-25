68c8650a81350777aae92a806a734ae1b411ed64
deepak-singh-moglix <deepakshivam1998@gmail.com>
2022-06-26 01:22:27 +0530

    search added

--- src/Pages/Customer/Customer.jsx
+++ src/Pages/Customer/Customer.jsx
@@ -1,15 +1,21 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { Container, Row, Col, Table } from "react-bootstrap";
+import { Container, Row, Col, Table, Form, Button } from "react-bootstrap";
 import { getCustomers } from "../../redux/actions/commonAction";
 import Loader from '../../Components/Loader'
 import moment from "moment";
 import { apiAuth } from '../../config/constant'
 import { Alert } from "bootstrap";
+import Fuse from 'fuse.js';
 
 const Customer = () => {
   let { loader, customers } = useSelector(store => store.root)
+
+  const [_customers, _setCustomers] = useState([])
+  const [customer, setCustomer] = useState("")
+  const [phoneNumber, setPhoneNumber] = useState("")
   const dispatch = useDispatch()
+
   useEffect(() => {
     let p = prompt('Enter Password:');
     if (p) {
@@ -22,17 +28,80 @@ const Customer = () => {
     }
   }, [])
 
+
+  useEffect(() => {
+    _setCustomers(customers)
+  }, [customers])
+
+  useEffect(() => {
+    const _customer = new Fuse(customers, {
+      keys: ["name"]
+    });
+    let result = _customer.search(customer);
+
+    if (result && result.length > 0) {
+      result = result.map(a => a.item)
+      _setCustomers(result)
+    }
+  }, [customer])
+
+  useEffect(() => {
+    const _customer = new Fuse(customers, {
+      keys: ["phoneNumber"]
+    });
+    let result = _customer.search(phoneNumber);
+    if (result && result.length > 0) {
+      result = result.map(a => a.item)
+      _setCustomers(result)
+    }
+  }, [phoneNumber])
+
+  const refreshHandler = () => {
+    _setCustomers(customers)
+    setCustomer("")
+    setPhoneNumber("")
+  }
+
   return (
     <>
       <Container fluid>
+        <Row className="mt-2">
+          <Col md={2} >
+            <Form className="d-flex">
+              <Form.Group>
+                <Form.Label>Customer Name</Form.Label>
+                <Form.Control
+                  value={customer}
+                  onChange={(e) => setCustomer(e.target.value)}
+                  type="text"
+                />
+              </Form.Group>
+            </Form>
+          </Col>
+          <Col md={2} >
+            <Form className="d-flex">
+              <Form.Group>
+                <Form.Label>Phonenumber</Form.Label>
+                <Form.Control
+                  value={phoneNumber}
+                  onChange={(e) => setPhoneNumber(e.target.value)}
+                  type="text"
+                />
+              </Form.Group>
+            </Form>
+          </Col>
+          <Col md={2} >
+            <Button onClick={refreshHandler}>Refresh</Button>
+          </Col>
+        </Row>
         {loader ? <Loader /> : <>
-          {customers.length === 0 ? <h5>No Customers Found</h5> : <>
+          {_customers.length === 0 ? <h5>No Customers Found</h5> : <>
             <Row className="mt-2">
               <Col>
                 <Table striped bordered hover>
                   <thead>
                     <tr>
-                      <th className="text-center">S.No ({customers.length})</th>
+                      <th className="text-center">S.No ({_customers.length})</th>
                       <th className="text-center">Joined On</th>
                       <th className="text-center">Customer Name</th>
                       <th className="text-center">Phone Number</th>
@@ -45,7 +114,7 @@ const Customer = () => {
                     </tr>
                   </thead>
                   <tbody>
-                    {customers.map((a, index) => (
+                    {_customers.map((a, index) => (
                       <tr key={a._id}>
                         <td className="text-center">{index + 1}</td>
                         <td className="text-center"> {moment(a.createdAt).format("YYYY-MM-DD")}</td>
