19ec5bbce73fc1e5a21a770673a412635dd80dae
deepak-singh-moglix <deepakshivam1998@gmail.com>
2022-10-02 01:14:58 +0530

    basic setup added

--- src/config/constant.js
+++ src/config/constant.js
@@ -1,6 +1,4 @@
-// const prod_url = "https://servimate-admin-prod.herokuapp.com/"
-const prod_url = "https://servimate-admin.herokuapp.com/"
-const prod_url1 = "https://servimate-admin.herokuapp.com/"
+const prod_url = "https://dev-servimate-admin.herokuapp.com/"
 const local_url = "http://localhost:4000/"
 const partnerWalletActionTypes = [{ "Add": "add" }, { "Subtract": "sub" }, { "Reset": "reset" }]
 const apiAuth = {
@@ -9,7 +7,6 @@ const apiAuth = {
 
 module.exports = {
     prod_url,
-    prod_url1,
     local_url,
     partnerWalletActionTypes,
     apiAuth
