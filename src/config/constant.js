d993d8d3cfab54f4670ec15c3bdc87ae8ac624c9
deepak-singh-moglix <deepakshivam1998@gmail.com>
2022-10-11 00:54:06 +0530

    aws source url added

--- src/config/constant.js
+++ src/config/constant.js
@@ -1,6 +1,8 @@
 // const prod_url = "https://servimate-admin-prod.herokuapp.com/"
-const prod_url = "https://servimate-admin.herokuapp.com/"
-const prod_url1 = "https://servimate-admin.herokuapp.com/"
+// const prod_url = "https://servimate-admin.herokuapp.com/"
+// const prod_url1 = "https://servimate-admin.herokuapp.com/"
+const prod_url = "http://appservimateserveradmin-env.eba-kkddpstp.us-east-1.elasticbeanstalk.com/"
+const prod_url1 = "http://appservimateserveradmin-env.eba-kkddpstp.us-east-1.elasticbeanstalk.com/"
 const local_url = "http://localhost:4000/"
 const partnerWalletActionTypes = [{ "Add": "add" }, { "Subtract": "sub" }, { "Reset": "reset" }]
 const apiAuth = {
