e92cf86c5e93239736ee82862e0e7999d45b3706
deepak-singh-moglix <deepakshivam1998@gmail.com>
2022-10-16 19:34:47 +0530

    config changed

--- src/config/constant.js
+++ src/config/constant.js
@@ -1,8 +1,6 @@
 // const prod_url = "https://servimate-admin-prod.herokuapp.com/"
 const prod_url = "https://servimate-admin.herokuapp.com/"
-const prod_url1 = "https://servimate-admin.herokuapp.com/"
 // const prod_url = "http://appservimateserveradmin-env.eba-kkddpstp.us-east-1.elasticbeanstalk.com/"
-// const prod_url1 = "http://appservimateserveradmin-env.eba-kkddpstp.us-east-1.elasticbeanstalk.com/"
 const local_url = "http://localhost:4000/"
 const partnerWalletActionTypes = [{ "Add": "add" }, { "Subtract": "sub" }, { "Reset": "reset" }]
 const apiAuth = {
@@ -11,7 +9,6 @@ const apiAuth = {
 
 module.exports = {
     prod_url,
-    prod_url1,
     local_url,
     partnerWalletActionTypes,
     apiAuth
