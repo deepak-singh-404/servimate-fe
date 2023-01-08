4a144781e505af99975650cc6f7a55372b08ea29
deepak-singh-moglix <deepakshivam1998@gmail.com>
2023-01-08 22:37:55 +0530

    city filter added

--- src/Pages/HomeScreen/Banner.js
+++ src/Pages/HomeScreen/Banner.js
@@ -1,25 +1,49 @@
 import React, { useState, useEffect } from "react";
-import { Container, Row, Col, Button, Table } from "react-bootstrap";
+import { Container, Row, Col, Button, Table, Form } from "react-bootstrap";
 import { useSelector, useDispatch } from 'react-redux'
 import { getBanners, setBanners } from '../../redux/actions/homeScreen'
 import DeleteModal from '../../Components/DeleteModal'
 import AddBannerModal from '../../Components/HomeScreen/AddBannerModal'
-import Loader  from "../../Components/Loader";
+import Loader from "../../Components/Loader";
+import { getCities } from "../../redux/actions/cityAction";
 
 const Banner = () => {
     const [addBannerModal, setAddBannerModal] = useState(false)
     const [data, setData] = useState("")
     const [deleteModal, setDeleteModal] = useState(false)
+    const [city, setCity] = useState("")
     const { loader, banners } = useSelector(state => state.homeScreenRoot)
+    const cities = useSelector((store) => store.cityRoot.cities)
     const dispatch = useDispatch()
+    const [_banners, _setBanners] = useState([])
 
     //GET ALL BANNERS
     useEffect(() => {
         dispatch(getBanners())
+        dispatch(getCities())
         return () => {
             setBanners([])
         }
     }, [])
+
+    //Initial Data
+    useEffect(() => {
+        if (banners) {
+            _setBanners(banners)
+        }
+    }, [banners])
+
+    //Filter As Per City
+    useEffect(() => {
+        if (city == "Select" || city == "") {
+            _setBanners(banners)
+        }
+        else {
+            const filteredData = banners.filter((b) => b?.city == city)
+            _setBanners(filteredData)
+        }
+    }, [city])
+
 
     const deleteHandler = (b) => {
         const temp_data = {
@@ -45,15 +69,26 @@ const Banner = () => {
             />}
             <Container>
                 <Row className="my-2">
-                    <Col >
+                    <Col md={2} >
                         <Button onClick={() => setAddBannerModal(true)}>
                             ADD BANNER
                         </Button>
                     </Col>
+                    <Col md={3} >
+                        <Form.Group >
+                            <Form.Label>City</Form.Label>
+                            <Form.Control onChange={(e) => setCity(e.target.value)} as="select">
+                                <option>Select</option>
+                                {cities.length !== 0 ? cities.map(c =>
+                                    <option value={c.name}>{c.name}</option>
+                                ) : null}
+                            </Form.Control>
+                        </Form.Group>
+                    </Col>
                 </Row>
                 <Row>
                     {loader ? <Loader /> : <>
-                        {banners.length === 0 ? <h5>No Banners Found</h5> : <>
+                        {_banners.length === 0 ? <h5>No Banners Found</h5> : <>
                             <Col  >
                                 <Table striped bordered hover>
                                     <thead>
@@ -65,7 +100,7 @@ const Banner = () => {
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        {banners.map((b, index) =>
+                                        {_banners.map((b, index) =>
                                             <tr>
                                                 <td className="text-center">{index + 1}</td>
                                                 <td className="text-center"><a href={b.picture} target="_blank">{b.picture && "picture"} </a></td>
